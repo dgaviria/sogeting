897d527a39275d29313a0a421365c8f5da002510
Michael <mars8082686@gmail.com>
2012-04-20 23:51:09 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sogaeting::Application.config.secret_token = 'a0ed212efc656a3549937518a4f93bdf399d435f718e92c8d7ef72453e48296b8c659564e705da8fb5dd625fff38877b6b36889468e6272ba59fe2fbfc52789f'
