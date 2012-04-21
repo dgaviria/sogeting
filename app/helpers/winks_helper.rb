5f9ef3da8f181af9def504e7662d4e74b93b3c4d
Dharmesh Patel <dbpatel219@gmail.com>
2012-04-21 19:44:54 -0400

    adding wink pages & update to user helper

--- app/helpers/winks_helper.rb
+++ app/helpers/winks_helper.rb
@@ -1,2 +1,16 @@
 module WinksHelper
+  def get_status(status)
+  	if status == "pending"
+		return "warning"
+	end
+	
+	if status == "approved"
+		return "success"
+	else 
+	    return "important"
+	end
+	
+	
+	
+  end
 end
